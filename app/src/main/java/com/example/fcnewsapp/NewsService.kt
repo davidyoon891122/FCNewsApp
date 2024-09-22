aaaff83ae2c6f48eea2b7ae6b32970ab0c830207
David Yoon <qqpo12@naver.com>
2024-09-22 15:03:05 +0900

    First Commit
    * Add libs(Retrofit, TikXml)
    * Added a Service and data class

---
+++ app/src/main/java/com/example/fcnewsapp/NewsService.kt
@@ -0,0 +1,12 @@
+package com.example.fcnewsapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewsService {
+
+    @GET("rss?hl=ko&gl=KR&ceid=KR:ko")
+    fun mainFeed(): Call<NewsRss>
+
+
+}
